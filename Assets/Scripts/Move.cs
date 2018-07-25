703a312c16e1454677b5a5aa7949fb1ddccc3bff
Cameron <cjbaker30@yahoo.com.au>
2018-07-25 10:32:41 +1000

    Initial commit

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+    public Rigidbody2D rb2D;
+    public float maxSpeed;
+    public float speed;
+   
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKey(KeyCode.W))
+        {
+            rb2D.AddForce(transform.up * speed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb2D.AddForce(-transform.up * speed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb2D.AddForce(-transform.right * speed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb2D.AddForce(transform.right * speed);
+        }
+    }
+}
