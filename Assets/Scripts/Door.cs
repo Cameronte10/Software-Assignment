8db0758f93f78941f9fecd6f25ef50cd5f7ac3d9
Cameronte10 <cjbaker30@yahoo.com.au>
2018-08-28 23:31:19 +1000

    making a kinda dent in the scripts
    aaaaaaahhhhhhh

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -2,29 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Door : MonoBehaviour {
+public class Door : MonoBehaviour
+{
     public int side;
     public RoomTemplate template;
-	// Use this for initialization
-	void Start () {
-		if(side == 0)
+    bool allowed = false;
+    // Use this for initialization
+    void Start()
+    {
+        if (side == 0)
         {
             //raycast up if raycastinfo != template.arraySide[] delete(gameObject)
             Vector2 fwd = transform.TransformDirection(Vector2.up);
             RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.up);
-            if (hit.collider != null && hit.transform.gameObject.CompareTag("Room") && )
+            for (int i = 0; i < template.leftRooms.Length; i++)
             {
-                
+                if (hit.transform.gameObject == template.leftRooms[i])
+                {
+                    allowed = true;
+                }
+                if (hit.collider != null && template.leftRooms[i] != hit.transform.gameObject)
+                {
+
+                }
             }
-                print("There is something in front of the object!");
+            print("There is something in front of the object!");
         }
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         Vector2 fwd = transform.TransformDirection(Vector2.up);
         Debug.DrawRay(transform.position, fwd, Color.blue);
-	}
+    }
     void FixedUpdate()
     {
 
